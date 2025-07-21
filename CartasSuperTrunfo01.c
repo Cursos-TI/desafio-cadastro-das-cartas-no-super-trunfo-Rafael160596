e25226c0e4f33bc21d81b394dc91c9d1768e633a
Rafael160596 <nicolasrafaelataidesmoura@gmail.com>
2025-07-21 23:58:00 +0000

    Inicio de variaveis

---
+++ CartasSuperTrunfo01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){ //Declarando variaveis.  
+
+    char estado;  // Estado.
+    char codigo[20]; //Codigo da carta.
+    char nome[20]; //Nome da cidade.
+    int população; //População da cidade.
+    float area; //Area em Km²
+    float PIB; //Produto Interno Bruto.
+    int pontos; //Pontos turisticos da cidade.
+
+    printf("Carta 01: \n");
+    printf("Digite a primeira letra do estado: ");
+}
