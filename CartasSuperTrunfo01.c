5045b4408c5925fcc80e4aa8ea3c43413541e1f1
Rafael160596 <nicolasrafaelataidesmoura@gmail.com>
2025-07-23 23:04:28 +0000

    Versão 01

--- CartasSuperTrunfo01.c
+++ CartasSuperTrunfo01.c
@@ -1,44 +1,83 @@
 #include <stdio.h>
 
-int main(){ //Declarando variaveis.  
+int main(){ 
+    //Declarando variaveis da primeira carta.  
+    char estado,codigo[50];
+    char nome[50];
+    int população, pontos;
+    float area, PIB;
 
-    char estado;  // Estado.
-    char codigo[3]; //Codigo da carta.
-    char nome[10]; //Nome da cidade.
-    int população; //População da cidade.
-    float area; //Area em Km² (decidi trabalhar com o double aceita mais valores =)
-    float PIB; //Produto Interno Bruto.
-    int pontos; //Pontos turisticos da cidade.
+    //Declarando variaveis da segunda carta.  
+    char estado2,codigo2[50];
+    char nome2[50];
+    int população2, pontos2;
+    float area2, PIB2;
 
-    //Entrada de dados com a interação com usuario
-    printf("Digite a primeira letra do estado de sua carta: \n");
-    scanf("%c", &estado);
+     //Entrada de dados com a interação com usuario Carta01.
+     printf("Carta 01: \n");
+     printf("Digite a primeira letra do estado de sua carta: \n");
+     scanf(" %c", &estado);
 
-    printf("Digite o codigo da cidade começando com a primeira letra do estado escolhido EX(F02): \n");
-    scanf("%s", &codigo);
+     printf("Digite o codigo da cidade começando com a primeira letra do estado escolhido EX(F02): \n");
+     scanf("%s", codigo);
 
-    printf("Digite o nome de sua cidade escolhida: \n");
-    scanf("%s", &nome);
+     printf("Digite o nome de sua cidade escolhida: \n");
+     scanf("%s", nome);
 
-    printf("Digite sua população: \n");
-    scanf("%d", &população);
+     printf("Digite sua população: \n");
+     scanf("%d", &população);
 
-    printf("Digite sua area: \n");
-    scanf("%f", &area);
+     printf("Digite sua area KM²: \n");
+     scanf("%f", &area);
 
-    printf("Digite seu PIB: \n");
-    scanf("%f", &PIB);
+     printf("Digite seu PIB: \n");
+     scanf("%f", &PIB);
 
-    printf("Digite sua quantidade de pontos turisticos: ");
-    scanf("%d", &pontos);
+     printf("Digite sua quantidade de pontos turisticos: \n");
+     scanf("%d", &pontos);
 
-    printf(" Carta 01: \n");
-    printf("Estado: %c\n", estado);
-    printf("Codigo: %s\n", codigo);
-    printf("Nome: %s\n", nome);
-    printf("Populução: %d\n", população);
-    printf("Area: %f KM²\n", area);
-    printf("PIB: %f \n", PIB);
-    printf("Pontos Turisticos: %d\n", pontos);
-    
+     //Entrada de dados com a interação com usuario Carta02.
+     printf("Carta 02: \n");
+     printf("Digite a primeira letra do estado de sua carta: \n");
+     scanf(" %c", &estado2);
+
+     printf("Digite o codigo da cidade começando com a primeira letra do estado escolhido EX(F02): \n");
+     scanf("%s", codigo2);
+
+     printf("Digite o nome de sua cidade escolhida: \n");
+     scanf("%s", nome2);
+
+     printf("Digite sua população: \n");
+     scanf("%d", &população2);
+
+     printf("Digite sua area KM²: \n");
+     scanf("%f", &area2);
+
+     printf("Digite seu PIB: \n");
+     scanf("%f", &PIB2);
+
+     printf("Digite sua quantidade de pontos turisticos: \n");
+     scanf("%d", &pontos2);
+
+        //Exibindo resultados Carta 01 (saida dos dados).
+         printf("Carta 01: \n");
+         printf("Estado: %c \n", estado);
+         printf("Codigo: %s \n", codigo);
+         printf("Nome: %s \n", nome);
+         printf("População: %d \n", população);
+         printf("Area: %f \n", area);
+         printf("PIB: %f \n", PIB);
+         printf("Pontos Turisticos: %d \n", pontos);
+         
+        //Exibindo resultados Carta 02 (saida dos dados).
+         printf("Carta 02: \n");
+         printf("Estado: %c \n", estado2);
+         printf("Codigo: %s \n", codigo2);
+         printf("Nome: %s \n", nome2);
+         printf("População: %d \n", população2);
+         printf("Area: %f \n", area2);
+         printf("PIB: %f \n", PIB2);
+         printf("Pontos Turisticos: %d \n", pontos2);
+
+return 0;
     }
