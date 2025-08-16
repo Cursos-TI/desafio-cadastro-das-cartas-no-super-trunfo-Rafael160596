f4d0b353323b12398544270dae0d34b9867417c9
Rafael160596 <nicolasrafaelataidesmoura@gmail.com>
2025-08-16 19:05:29 +0000

    Desafiosupertrunfo

--- CartasSuperTrunfo01.c
+++ CartasSuperTrunfo01.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese_Brazil");
+    setlocale(LC_ALL, "Portuguese");
 
     // Variáveis da primeira carta
     char estado1, codigo1[20], nome1[50];
@@ -32,8 +33,11 @@ int main() {
     printf("Digite o código da cidade (ex: A01, B03): ");
     scanf("%s", codigo1);
 
+    while (getchar() != '\n'); //limpar o buffer antes do fgets
+
     printf("Digite o nome da cidade: ");
-    scanf(" %49[^\n]", nome1); //comando para ler nomes compostos
+    fgets(nome1, sizeof(nome1), stdin); //comando para ler nomes compostos
+    nome1[strcspn(nome1, "\n")] = 0; // remove o '\n' final
 
     printf("Digite o número de habitantes: ");
     scanf("%lu", &populacao1);
@@ -61,8 +65,11 @@ int main() {
     printf("Digite o código da cidade (ex: A01, B03): ");
     scanf("%s", codigo2);
 
+    while (getchar() != '\n'); //limpar o buffer antes do fgets
+
     printf("Digite o nome da cidade: ");
-    scanf(" %49[^\n]", nome2);
+    fgets(nome2, sizeof(nome2), stdin);
+    nome2[strcspn(nome2, "\n")] = 0; // remove o '\n' final 
 
     printf("Digite o número de habitantes: ");
     scanf("%lu", &populacao2);
@@ -90,10 +97,9 @@ int main() {
          printf("Área: %.2f KM²\n", area1);
          printf("PIB: %.2f\n", pib1);
          printf("Pontos Turísticos: %d\n", pontosturisticos1);
-         printf("Densidade Populacional: %.2f\n", densidadepopulacional1);
-         printf("PIB per Capita: %.2f\n", pibpercapta1);
-         printf("Super Poder: %.2f\n", superpoder1);
-
+         printf("Densidade Populacional: %.2f hab/km² \n", densidadepopulacional1);
+         printf("PIB per Capita: %.2f reais \n", pibpercapta1);
+         
         // Exibindo resultados - Carta 2
          printf("\n=== Carta 02 ===\n");
          printf("Estado: %c\n", estado2);
@@ -103,20 +109,19 @@ int main() {
          printf("Área: %.2f KM²\n", area2);
          printf("PIB: %.2f\n", pib2);
          printf("Pontos Turísticos: %d\n", pontosturisticos2);
-         printf("Densidade Populacional: %.2f\n", densidadepopulacional2);
-         printf("PIB per Capita: %.2f\n", pibpercapta2);
-         printf("Super Poder: %.2f\n", superpoder2);
-
+         printf("Densidade Populacional: %.2f hab/km² \n", densidadepopulacional2);
+         printf("PIB per Capita: %.2f reais \n", pibpercapta2);
+         
              // Comparação de cartas
              printf("\n=== Comparação de Cartas ===\n");
-             printf("(1 para verdadeiro – Carta 1 vence – e 0 para falso – Carta 2 vence)\n");
-             printf("População: %d\n", populacao1 > populacao2 ? 1 : 0);
-             printf("Área: %d\n", area1 > area2 ? 1 : 0);
-             printf("PIB: %d\n", pib1 > pib2 ? 1 : 0);
-             printf("Pontos Turísticos: %d\n", pontosturisticos1 > pontosturisticos2 ? 1 : 0);
-             printf("Densidade Populacional: %d\n", densidadepopulacional1 < densidadepopulacional2 ? 1 : 0);
-             printf("PIB per Capita: %d\n", pibpercapta1 > pibpercapta2 ? 1 : 0);
-             printf("Super Poder: %d\n", superpoder1 > superpoder2 ? 1 : 0);
+             printf("1 para verdadeiro = Carta 1 vence / e 0 para falso = Carta 2 vence\n");
 
-    return 0;
+             printf("População: Carta 1 venceu ? (%d)\n", populacao1 > populacao2);
+             printf("Area: Carta 1 venceu ? (%d)\n", area1 > area2);
+             printf("PIB: Carta 1 venceu ? (%d)\n", pib1 > pib2);
+             printf("Pontos Turísticos: Carta 1 venceu ? (%d)\n", pontosturisticos1 > pontosturisticos2);
+             printf("Densidade Populacional: Carta 1 venceu ? (%d)\n", densidadepopulacional1 < densidadepopulacional2);
+             printf("PIB per Capita: Carta 1 venceu ? (%d)\n", pibpercapta1 > pibpercapta2);
+             printf("Super Poder: Carta 1 venceu ? (%d)\n", superpoder1 > superpoder2);          
+ return 0;
 }
